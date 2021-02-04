c480156af793a7e1a471276f0e8070db6a1985aa
Johann Bugge <60435883+Johannbugge@users.noreply.github.com>
2021-02-04 09:34:42 +0100

    \

--- dat110-project1/src/no/hvl/dat110/messaging/Connection.java
+++ dat110-project1/src/no/hvl/dat110/messaging/Connection.java
@@ -32,22 +32,22 @@ public class Connection {
 
 	public void send(Message message) {
 
-		// TODO
-		// encapsulate the data contained in the message and write to the output stream
-		// Hint: use the encapsulate method on the message
-		throw new UnsupportedOperationException(TODO.method());
-
+		try {
+			outStream.write(message.encapsulate());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public Message receive() {
 
-		Message message;
-		byte[] recvbuf;
+		Message message = new Message();
+		byte[] recvbuf = new byte[SEGMENTSIZE];
 
 		// TODO
 		// read a segment (128 bytes) from the input stream and decapsulate into message
 		// Hint: create a new Message object and use the decapsulate method
-		
+
 		if (true) {
 			throw new RuntimeException("not yet implemented");
 		}
